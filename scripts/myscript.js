1627559946e2226b6776a012b00f329c102787cb
Yenifer30 <yrivas@utp.edu.co>
2019-08-14 18:28:56 -0500

    js and json

--- scripts/myscript.js
+++ scripts/myscript.js
@@ -5,14 +5,14 @@ function load() {
   var nombre1 = document.getElementById('nombre');
   var nombre2 = document.getElementById('nombre4');
   var referencia2 = document.getElementById('referencia4');
-  var nombre3 = document.getElementById('nombre11');
-  var referencia3 = document.getElementById('referencia11');
+  // var nombre3 = document.getElementById('nombre11');
+  // var referencia3 = document.getElementById('referencia11');
   referencia1.innerHTML = mydata[8].referencia;
   nombre1.innerHTML = mydata[8].nombre;
   referencia2.innerHTML = mydata[4].referencia4;
   nombre2.innerHTML = mydata[4].nombre4;
-  referencia3.innerHTML = mydata[10].referencia11;
-  nombre3.innerHTML = mydata[10].nombre11;
+  // referencia3.innerHTML = mydata[10].referencia11;
+  // nombre3.innerHTML = mydata[10].nombre11;
   // for (var x in mydata) {
   //
   // }
